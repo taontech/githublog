672e684a1c80e61616a38ca0d98ea1ff92de0e7b
taoning001 <taoning001@ke.com>
2022-04-16 11:49:01 +0800

    大图显示样式调整

--- commonmark.js
+++ commonmark.js
@@ -9837,9 +9837,9 @@
         if (entering) {
             if (this.disableTags === 0) {
                 if (this.options.safe && potentiallyUnsafe(node.destination)) {
-                    this.lit('<img src="" max-width=100% alt=" ');
+                    this.lit('<img src="" style="max-width: 100%;" alt=" ');
                 } else {
-                    this.lit('<img src="' + this.esc(node.destination) + '" max-width=100% alt="');
+                    this.lit('<img src="' + this.esc(node.destination) + '" style="max-width: 100%;" alt="');
                 }
             }
             this.disableTags += 1;
